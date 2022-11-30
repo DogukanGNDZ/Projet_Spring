5f54175568ae889d999bb15ab851c7af42f32b69
dogukan <dogukan.gunduz@student.vinci.be>
2022-11-30 13:51:40 +0100

    gateway : users part

--- gateway/src/main/java/ipl/vinci/be/gateway/GatewayController.java
+++ gateway/src/main/java/ipl/vinci/be/gateway/GatewayController.java
@@ -1,13 +1,13 @@
 package ipl.vinci.be.gateway;
 
 
-import ipl.vinci.be.gateway.models.Credentials;
-import ipl.vinci.be.gateway.models.User;
-import ipl.vinci.be.gateway.models.UserWithCredentials;
+import ipl.vinci.be.gateway.models.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @CrossOrigin(origins = {"https://www.ChattyCar.com", "http://localhost"})
 @RestController
@@ -44,4 +44,61 @@ public class GatewayController {
         service.updateUserPwd(user);
 
     }
+    @GetMapping("/users/{id}")
+    User readUserById(@PathVariable int id, @RequestHeader("Authorization") String token){
+
+        String userEmail = service.verify(token);
+        User user = service.readUserById(id);
+        if (!user.getEmail().equals(userEmail)) throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        return user;
+    }
+    @PutMapping("/users/{id}")
+    void updateUserById(@PathVariable int id,@RequestBody User user, @RequestHeader("Authorization") String token){
+
+        String userEmail = service.verify(token);
+        if (!user.getEmail().equals(userEmail)) throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        service.updateUser(id,user);
+    }
+    @DeleteMapping("/users/{id}")
+    void deleteUser(@PathVariable int id, @RequestHeader("Authorization") String token){
+        String userEmail = service.verify(token);
+        User user = service.readUserById(id);
+        if (!user.getEmail().equals(userEmail)) throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        service.deleteUser(id);
+    }
+    /*
+    ligne 157 du YAML
+    @GetMapping("/users/{id}/driver")
+    List<Trip> UserTripDriver(@PathVariable int id, @RequestHeader("Authorization") String token){
+        //besoin de trip je suis agenoux
+    }
+
+    */
+
+/*
+
+ligne 187 du YML
+    @GetMapping("/users/{id}/passenger")
+    Passenger_trips UserTripDriver(@PathVariable int id, @RequestHeader("Authorization") String token){
+        //besoin de passenger je suis agenoux
+    }
+*/
+
+    @GetMapping("/users/{id}/notifications")
+    List<Notification> getUserNotification(@PathVariable int id, @RequestHeader("Authorization") String token){
+        String userEmail = service.verify(token);
+        User user = service.readUserById(id);
+        if (!user.getEmail().equals(userEmail)) throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        return (List<Notification>) service.getNotification(id);
+    }
+    @DeleteMapping("/users/{id}/notifications")
+    void deleteUserNotif(@PathVariable int id, @RequestHeader("Authorization") String token){
+        String userEmail = service.verify(token);
+        User user = service.readUserById(id);
+        if (!user.getEmail().equals(userEmail)) throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        service.deleteUserNotif(id);
+    }
+
+
+
 }
--- gateway/src/main/java/ipl/vinci/be/gateway/GatewayService.java
+++ gateway/src/main/java/ipl/vinci/be/gateway/GatewayService.java
@@ -4,9 +4,12 @@ import ipl.vinci.be.gateway.data.AuthenticationProxy;
 import ipl.vinci.be.gateway.data.NotificationsProxy;
 import ipl.vinci.be.gateway.data.UsersProxy;
 import ipl.vinci.be.gateway.models.Credentials;
+import ipl.vinci.be.gateway.models.Notification;
 import ipl.vinci.be.gateway.models.User;
 import ipl.vinci.be.gateway.models.UserWithCredentials;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GatewayService {
@@ -49,4 +52,18 @@ public class GatewayService {
     public User readUser(String email) {
         return usersProxy.readOneByEmail(email);
     }
+    public User readUserById(int id) {
+        return usersProxy.readOneById(id);
+    }
+    public void updateUser(long id,User user) {
+        usersProxy.updateOne(id, user);
+    }
+
+    public Iterable<Notification> getNotification(long id){
+      return  notificationsProxy.getUserNotifications(id);
+    }
+    public void deleteUserNotif(long id){
+        notificationsProxy.deleteAllUserNotifications(id);
+    }
+
 }
