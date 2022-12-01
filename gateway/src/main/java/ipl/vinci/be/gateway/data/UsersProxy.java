7041934b25f850752c72ac60c30c5a8e626e4f13
Jérôme Devaux <jerome.devaux@student.vinci.be>
2022-12-01 19:19:33 +0100

    OMGGG

--- gateway/src/main/java/ipl/vinci/be/gateway/data/UsersProxy.java
+++ gateway/src/main/java/ipl/vinci/be/gateway/data/UsersProxy.java
@@ -9,21 +9,21 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 @Repository
-@FeignClient(name = "user")
+@FeignClient(name = "users")
 public interface UsersProxy {
 
     @PostMapping("/users")
-    public User createOne(@RequestBody NewUser user);
+    User createOne(@RequestBody NewUser user);
 
     @GetMapping("/users")
-    public User readOneByEmail (@RequestParam String email);
+    User readOneByEmail (@RequestParam String email);
 
     @GetMapping("/users/{id}")
-    public User readOneById (@PathVariable long id);
+    User readOneById (@PathVariable long id);
 
     @PutMapping("/users/{id}")
-    public void updateOne(@PathVariable long id, @RequestBody User user);
+    void updateOne(@PathVariable long id, @RequestBody User user);
 
     @DeleteMapping("/users/{id}")
-    public void deleteOne(@PathVariable long id);
+    void deleteOne(@PathVariable long id);
 }
--- passenger/src/main/java/be/vinci/ipl/passenger/PassengerController.java
+++ passenger/src/main/java/be/vinci/ipl/passenger/PassengerController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -39,7 +40,7 @@ public class PassengerController {
   }
 
   @PutMapping("/passengers/{trip_id}/{user_id}")
-  public ResponseEntity<Void> updatePassenger(@PathVariable long trip_id, @PathVariable long user_id, @RequestParam String etat){
+  public ResponseEntity<Void> updatePassenger(@PathVariable long trip_id, @PathVariable long user_id, @RequestBody String etat){
     if (!(etat == "ACCEPTED" || etat == "REFUSED")){throw new StatusNotInAcceptedValueException();}
     service.updateStatus(trip_id, user_id, etat);
     return new ResponseEntity<>(HttpStatus.OK);
